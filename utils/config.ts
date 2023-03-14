2f832423a91b3a37716ce8a39a89db50b75f50cd
kaizziizg <63266693+kaizziizg@users.noreply.github.com>
2023-03-15 03:12:07 +0800

    fix: fix cloud SQL connect

--- utils/config.ts
+++ utils/config.ts
@@ -3,6 +3,6 @@ const user: string = process.env.ShortUrlUsername as string
 const pwd: string = process.env.ShortUrlPassword as string
 const INSTANCE_CONNECTION_NAME: string = process.env.INSTANCE_CONNECTION_NAME as string
 const database: string = 'shortUrlService'
-// console.log(`env\n host: ${host}\n user: ${user}\n pwd: ${pwd}`)
+console.log(`env\n host: ${INSTANCE_CONNECTION_NAME}\n user: ${user}\n pwd: ${pwd}`)
 
 export { host, user, pwd, database, INSTANCE_CONNECTION_NAME }
--- utils/db/db_init.ts
+++ utils/db/db_init.ts
@@ -3,7 +3,6 @@ import * as env from '../config.js'
 
 const saltRounds = 10
 
-
 // const sequelize = new Sequelize(env.database, env.user, env.pwd, {
 //   in common environment
 //   host: env.host,
@@ -15,8 +14,14 @@ const saltRounds = 10
 
 // in GCP
 const sequelize = new Sequelize(env.database, env.user, env.pwd, {
-  host: `/cloudsql/${env.INSTANCE_CONNECTION_NAME}`,
   dialect: 'mysql',
+  host: `/cloudsql/${env.INSTANCE_CONNECTION_NAME}`,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
   dialectOptions: {
     // e.g. socketPath: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
     // same as host string above
