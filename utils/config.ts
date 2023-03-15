1782d630966682b98d8531b64b0e197addbf8a48
kaizziizg <63266693+kaizziizg@users.noreply.github.com>
2023-03-15 13:37:26 +0800

    feat: add environment checking,GCP env must add Env-Variable with isGcpEnv=true

--- utils/config.ts
+++ utils/config.ts
@@ -3,6 +3,14 @@ const user: string = process.env.ShortUrlUsername as string
 const pwd: string = process.env.ShortUrlPassword as string
 const INSTANCE_CONNECTION_NAME: string = process.env.INSTANCE_CONNECTION_NAME as string
 const database: string = 'shortUrlService'
-console.log(`env\n host: ${INSTANCE_CONNECTION_NAME}\n user: ${user}\n pwd: ${pwd}`)
+const isGcpEnv: boolean = (process.env.isGcpEnv === 'true')
 
-export { host, user, pwd, database, INSTANCE_CONNECTION_NAME }
+if (isGcpEnv) {
+  console.log('Current Environment is GCP, Connect SQl with Unix sockets')
+  console.log(`env\n host: ${INSTANCE_CONNECTION_NAME}\n user: ${user}\n pwd: ${pwd}`)
+} else {
+  console.log('Current Environment is general, Connect SQl with TCP')
+  console.log(`env\n host: ${host}\n user: ${user}\n pwd: ${pwd}`)
+}
+
+export { host, user, pwd, database, INSTANCE_CONNECTION_NAME, isGcpEnv }
--- utils/db/db_init.ts
+++ utils/db/db_init.ts
@@ -2,34 +2,36 @@ import { Sequelize } from 'sequelize'
 import * as env from '../config.js'
 
 const saltRounds = 10
+const isGcpEnv: boolean = env.isGcpEnv
 
-// const sequelize = new Sequelize(env.database, env.user, env.pwd, {
-//   in common environment
-//   host: env.host,
-//   dialect: 'mysql',
-//   define: {
-//     timestamps: false
-//   }
-// })
-
-// in GCP
-const sequelize = new Sequelize(env.database, env.user, env.pwd, {
+let sequelize = new Sequelize(env.database, env.user, env.pwd, {
+  host: env.host,
   dialect: 'mysql',
-  host: `/cloudsql/${env.INSTANCE_CONNECTION_NAME}`,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  dialectOptions: {
-    // e.g. socketPath: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
-    // same as host string above
-    socketPath: `/cloudsql/${env.INSTANCE_CONNECTION_NAME}`
-  },
   define: {
     timestamps: false
   }
 })
+
+if (isGcpEnv) {
+// in GCP
+  sequelize = new Sequelize(env.database, env.user, env.pwd, {
+    dialect: 'mysql',
+    host: `/cloudsql/${env.INSTANCE_CONNECTION_NAME}`,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    dialectOptions: {
+    // e.g. socketPath: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
+    // same as host string above
+      socketPath: `/cloudsql/${env.INSTANCE_CONNECTION_NAME}`
+    },
+    define: {
+      timestamps: false
+    }
+  })
+}
 
 export { sequelize, saltRounds }
