1bec0be286083bd91fbc3468df0077044aac8ff5
kaizziizg <63266693+kaizziizg@users.noreply.github.com>
2023-04-02 13:46:41 +0800

    fix: update new ServerIP

--- reactjs/src/config.js
+++ reactjs/src/config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 const isLocalServer = false;
-const serverIP = isLocalServer ? 'http://127.0.0.1:3000' : 'https://shorteningurl-eh4konhfta-de.a.run.app';
+const serverIP = isLocalServer ? 'http://127.0.0.1:3000' : 'https://shorteningurl-tvuvdrqvfq-de.a.run.app';
 // set isDev to view log
 const isDev = false;
 function clog(msg) {
